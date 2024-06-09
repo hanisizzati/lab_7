000a09a5cb9010e48bf585db753f76c411ec1a84
Hanis Izzati <hanisizzati613@gmail.com>
2024-06-10 07:51:11 +0800

    Initial commit

--- update.php
+++ update.php
@@ -1,38 +1,18 @@
-<!DOCTYPE html>
-<html lang='eng'>
-<head>
-    <title>Update</title>
-</head>
-<body>
 <?php
+include 'Database.php';
+include 'User.php';
 
-$users = array(
-    array("matric" => "02000", "name" => "Nur Ariffin Mohd Zin", "level" => "lecturer", "action" => "<a href='update.php?id=02000'>Update</a> | <a href='delete.php?id=02000'>Delete</a>"),
-    array("matric" => "A100", "name" => "Ahmad", "level" => "student", "action" => "<a href='update.php?id=A100'>Update</a> | <a href='delete.php?id=A100'>Delete</a>"),
-    array("matric" => "A101", "name" => "Abu", "level" => "student", "action" => "<a href='update.php?id=A101'>Update</a> | <a href='delete.php?id=A101'>Delete</a>"),
-    array("matric" => "A103", "name" => "Ahmad bin Abu", "level" => "student", "action" => "<a href='update.php?id=A103'>Update</a> | <a href='delete.php?id=A103'>Delete</a>")
-);
-
-?>
-<table border="1">
-    <tr>
-        <th>Matric</th>
-        <th>Name</th>
-        <th>Level</th>
-        <th>Action</th>
-    </tr>
-    <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
-    foreach ($users as $user) {
-        echo "<tr>";
-        echo "<td>" . $user["matric"] . "</td>";
-        echo "<td>" . $user["name"] . "</td>";
-        echo "<td>" . $user["level"] . "</td>";
-        echo "<td>" . $user["action"] . "</td>";
-        echo "</tr>";
-    }
-    ?>
-</table>
+    $matric = $_POST['matric'];
+    $name = $_POST['name'];
+    $role = $_POST['role'];
 
-</body>
-</html>
+    $database = new Database();
+    $db = $database->getConnection();
+
+    $user = new User($db);
+    $user->updateUser($matric, $name, $role);
+
+    $db->close();
+}
