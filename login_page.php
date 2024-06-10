8b5a240e6ad9b8b5eda775b211519db2260a1848
Hanis Izzati <hanisizzati613@gmail.com>
2024-06-10 16:46:41 +0800

    Initial commit

--- login_page.php
+++ login_page.php
@@ -4,18 +4,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel=stylesheet href="style.css">
+    <title>Login Page</title>
 </head>
 
 <body>
-    <h1>Login Page</h1>
-    <form action="authenticate.php" method="post">
+    <form action="login.php" method="post">
         <label for="matric">Matric:</label>
         <input type="text" name="matric" id="matric" required><br>
+
         <label for="password">Password:</label>
         <input type="password" name="password" id="password" required><br>
-        <input type="submit" name="submit" value="Submit">
-    </form>
-</body>
 
+    <input type="submit" name="submit" value="Register">
+</form>
+
+<p><a href="registration_page.php">Register</a> here if you have not. </p>
+
+</body>
 </html>
