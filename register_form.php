97aada4c836a0efc2512c6149f9fb9aff62eec0f
Hanis Izzati <hanisizzati613@gmail.com>
2024-06-10 16:46:55 +0800

    Initial commit

--- register_form.php
+++ register_form.php
@@ -4,26 +4,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Registration Page</title>
 </head>
 
 <body>
-    <form action="insert.php" method="post">
+    <form action="registration.php" method="post">
         <label for="matric">Matric:</label>
         <input type="text" name="matric" id="matric" required><br>
-        <label for="name">Name:</label>
-        <input type="text" name="name" id="name" required><br>
+
+        <label for ="name">Name:</label>
+        <input type="text" name="name" id"name" required><br>
+
         <label for="password">Password:</label>
         <input type="password" name="password" id="password" required><br>
+
         <label for="role">Role:</label>
         <select name="role" id="role" required>
-            <option value="">Please select</option>
+            <option value="">Select</option>
             <option value="lecturer">Lecturer</option>
             <option value="student">Student</option>
-        </select><br>
-        <input type="submit" name="submit" value="Register">
-    </form>
+    </select><br>
 
+    <input type="submit" name="submit" value="Submit">
+
+</form>
 </body>
-
 </html>
